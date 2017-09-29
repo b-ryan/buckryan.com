93873f9917dd98ce321179c241c723eccd96d8ec
Buck Ryan <buck.ryan2@gmail.com>
2017-09-29 15:43:38 -0400

    redo the whole theme

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
+AUTHOR = u"Buck Ryan"
+SITENAME = u"buckryan.com"
+SITESUBTITLE = u""
+SITEURL = "http://localhost:8888"
 
-AUTHOR = u'Buck Ryan'
-SITENAME = u'buckryan.com'
-SITESUBTITLE = u''
-SITEURL = ''
+TIMEZONE = "America/New_York"
 
-TIMEZONE = 'America/New_York'
-
-DEFAULT_LANG = u'en'
+DEFAULT_LANG = u"en"
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
@@ -17,17 +13,17 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 
 # Blogroll
-LINKS =  (('Pelican', 'http://getpelican.com/'),
-          ('Python.org', 'http://python.org/'),
-          ('Jinja2', 'http://jinja.pocoo.org/'),
-          ('You can modify those links in your config file', '#'),)
+LINKS =  (("Pelican", "http://getpelican.com/"),
+          ("Python.org", "http://python.org/"),
+          ("Jinja2", "http://jinja.pocoo.org/"),
+          ("You can modify those links in your config file", "#"),)
 
-SOCIAL = (('twitter', 'https://twitter.com/_buckryan'),
-          ('github', 'https://github.com/b-ryan'),)
+SOCIAL = (("twitter", "https://twitter.com/_buckryan"),
+          ("github", "https://github.com/b-ryan"),)
 
 DEFAULT_PAGINATION = 10
 
-STATIC_PATHS = ['files']
+STATIC_PATHS = ["files"]
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
@@ -37,4 +33,44 @@ DISPLAY_CATEGORIES_ON_MENU = False
 DISPLAY_AUTHOR = False
 DISPLAY_FOOTER = False
 
-THEME = "theme"
+THEME = "flex"
+PATH = "content"
+SITELOGO = "/static/me.png"
+# FAVICON = "https://www.dbrhino.com/theme/img/dbrhino-logo.ico"
+ROBOTS = "index, follow"
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa"
+}
+COPYRIGHT_YEAR = 2017
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
+SOCIAL = (("github", "https://github.com/b-ryan"),
+          ("twitter", "https://twitter.com/_buckryan"),
+          ("rss", "//www.buckryan.com/feeds/all.atom.xml"))
+
+SITEMAP = {
+    "format": "xml",
+    "priorities": {
+        "articles": 0.6,
+        "indexes": 0.6,
+        "pages": 0.5,
+    },
+    "changefreqs": {
+        "articles": "monthly",
+        "indexes": "daily",
+        "pages": "monthly",
+    }
+}
+MAIN_MENU = True
+MENUITEMS = (("Archives", "/archives.html"),
+             ("Categories", "/categories.html"),
+             ("Tags", "/tags.html"),)
+
+STATIC_PATHS = ["extra"]
+EXTRA_PATH_METADATA = {
+    "extra/me.png": {"path": "static/me.png"},
+    "extra/resume-2017-01.pdf": {"path": "static/resume-2017-01.pdf"},
+}
+LINKS = tuple()
--- publishconf.py
+++ publishconf.py
@@ -1,23 +1,16 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*- #
-from __future__ import unicode_literals
-
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://buckryan.com'
+SITEURL = "http://buckryan.com"
 RELATIVE_URLS = False
 
-FEED_ALL_ATOM = 'feeds/all.atom.xml'
-CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
+FEED_ALL_ATOM = "feeds/all.atom.xml"
+CATEGORY_FEED_ATOM = "feeds/%s.atom.xml"
 
 DELETE_OUTPUT_DIRECTORY = True
 
 CLICKY_ID = 100683631
-DISQUS_SITENAME = 'buckryan'
-#GOOGLE_ANALYTICS = ""
+DISQUS_SITENAME = "buckryan"
+GOOGLE_ANALYTICS = "UA-39435134-1"
